b71ad0ce36bc8975c908b4007ad8482f23e1d3ec
don <donaldokara1232@gmail.com>
2024-07-17 18:32:31 +0300

    fix: Fixed the bug that reset the user data to the placeholders on sign in

--- app/src/main/java/com/example/thejourney/presentation/communities/CommunityViewModel.kt
+++ app/src/main/java/com/example/thejourney/presentation/communities/CommunityViewModel.kt
@@ -1,4 +1,11 @@
-package com.example.thejourney.presentation.communities.campuses
+package com.example.thejourney.presentation.communities
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
 
 class CommunityViewModel {
 
